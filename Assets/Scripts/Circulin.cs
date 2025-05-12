512db1416c463cddc41cdce56f8314d5c5a7d3d5
Boerming933 <matiasdelprato@gmail.com>
2025-05-12 19:33:30 -0300

    Ataque Latigo, Movimiento y Ataque Onda jefe

---
+++ Assets/Scripts/Circulin.cs
@@ -0,0 +1,124 @@
+using UnityEngine;
+using System.Collections;
+using Unity.VisualScripting;
+
+public class Circulin : MonoBehaviour
+{   
+    public Animator anim;
+    public float meleeSpeed;
+    public float damage;
+    float timeUntilMelee;
+
+    public Transform Mira;
+    private bool canDash = true;
+    private bool isDashing;
+    public float dashingPower;
+    private float dashingTime = 0.1f;
+    private float dashingWait = 0.1f;
+    private float dashingCooldown = 2f;
+    public float JumpForce, speed;
+
+    private Rigidbody2D Rigidbody2D;
+    private TrailRenderer trailRenderer;
+    private float Horizontal;
+    private bool Grounded;
+
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        trailRenderer = GetComponent<TrailRenderer>();
+        
+    }
+
+    private void Update()
+    {
+        if(isDashing)
+        {
+            return;
+        }
+
+        if(timeUntilMelee <= 0f)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                anim.SetTrigger("Attack");
+                timeUntilMelee = meleeSpeed;
+            }
+        }
+        else
+        {
+            timeUntilMelee -= Time.deltaTime;
+        }
+        
+        Horizontal = Input.GetAxisRaw("Horizontal") * speed;
+
+        Vector3 origin = new Vector3(transform.position.x, transform.position.y - 0.43f, transform.position.z);
+        Debug.DrawRay(origin, Vector3.down * 0.2f, Color.red);
+        RaycastHit2D hit = Physics2D.Raycast(origin, Vector3.down, 0.2f);
+        if(hit.collider != null && hit.collider.CompareTag("Ground"))
+        {
+            Grounded = true;
+        }
+        else Grounded = false;
+        
+        if(Input.GetKeyDown(KeyCode.Space) && Grounded)
+        {
+            Jump();
+        }
+        Mira.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+        float angle = Mathf.Atan2(Mira.position.y - transform.position.y, Mira.position.x - transform.position.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0,0,angle);
+        
+        if(Input.GetKey(KeyCode.LeftShift) && canDash)
+        {   
+            StartCoroutine(Dash());
+        }        
+    }
+    
+    private void Jump()
+    {
+        Rigidbody2D.AddForce(Vector2.up*JumpForce);
+    }
+
+    private IEnumerator Dash()
+    {   
+        canDash = false;        
+        isDashing = true;
+        trailRenderer.emitting = true;           
+        float originalGravity = Rigidbody2D.gravityScale;
+        Rigidbody2D.gravityScale = 0f;
+        if(Horizontal >= 0f)
+        {
+            Rigidbody2D.linearVelocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        }
+        if(Horizontal < 0f)
+        {
+            Rigidbody2D.linearVelocity = new Vector2(transform.localScale.x * -1f * dashingPower, 0f);
+        }
+        yield return new WaitForSeconds(dashingTime);
+        trailRenderer.emitting = false;
+        Rigidbody2D.gravityScale = originalGravity;
+        yield return new WaitForSeconds(dashingWait);
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {     
+        if (other.tag == "Triangulardo")
+        {
+            //other.GetComponent<Triangulardo>().TakeDamage();
+            Debug.Log("Enemy Hit");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(isDashing)
+        {
+            return;
+        }
+        Rigidbody2D.linearVelocity = new Vector2(Horizontal, Rigidbody2D.linearVelocityY);
+    }
+}
