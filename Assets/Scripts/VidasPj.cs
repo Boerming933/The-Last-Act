11d8a6e0eef43a34a92bff9126825e76d4ff7d20
Zeku <juano.riverarozo@gmail.com>
2025-05-13 16:05:45 -0300

    Vidas + Boton
    -crear canvas, crear un empy dentro (contenedor de los corazones), y en propiedades ubicarlo esquina superior izquierda. Luego agregar el componente grid layout group a ese vacio.
    
    -crear dentro del vacio un UI > Image, y cargarle el sprite del corazon, ajustar el tamaño y luego duplicar hasta 5, luego configurar en las propiedades del objeto vacio la separacion, las filas, etc
    
    -Crear el boton en el medio del canvas y destildar la opcion de que inicie activado, el script hara el resto

---
+++ Assets/Scripts/VidasPj.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
+
+public class VidasPj : MonoBehaviour
+{
+    public int Vidas = 5;
+    public GameObject BotonReiniciar;
+
+    public List<GameObject> IndicadoresDeVida; //Aqui van los 5 corazones de la UI
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Triangulardo")
+        {
+            Vidas -= 1;
+
+            ActualizarIndicadoresDeVida();
+
+            if (Vidas <= 0)
+            {
+                gameObject.SetActive(false);
+                ActivarBotonReinicio();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Ataques")
+        {
+            Vidas -= 1;
+
+            ActualizarIndicadoresDeVida();
+
+            if (Vidas <= 0)
+            {
+                gameObject.SetActive(false);
+                ActivarBotonReinicio();
+            }
+        }
+    }
+
+
+    public void ActualizarIndicadoresDeVida()
+    {
+        if (IndicadoresDeVida.Count > 0)
+        {
+            // Desactiva el ultimo (de derecha a izquierda)
+            int ultimoIndice = IndicadoresDeVida.Count - 1;
+            IndicadoresDeVida[ultimoIndice].SetActive(false);
+
+            // Lo quita de la lista
+            IndicadoresDeVida.RemoveAt(ultimoIndice);
+        }
+    }
+
+    public void ActivarBotonReinicio()
+    {
+        BotonReiniciar.SetActive(true);
+        BotonReiniciar.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(ReiniciarEscena);
+    }
+
+    public void ReiniciarEscena()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
